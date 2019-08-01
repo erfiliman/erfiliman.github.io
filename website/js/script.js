c91ce49ab4a11d61ee4bff144c4c978a0d89dc64
erfiliman <53553718+erfiliman@users.noreply.github.com>
2019-08-01 20:44:51 +0700

    slider comlite

--- website/js/script.js
+++ website/js/script.js
@@ -2,4 +2,15 @@ $(function(){
 	$('.list-categories__item').on('click', function(){
 		$(this).find("ul:first").slideToggle();
 	});
+
+	$('.wallpaper-item').on('click', function() {
+		var bg = $(this).css('background-image');
+		bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+		$('.modal-wallpaper-show__item').attr('src', bg );
+		$('.modal-wallpaper-show').fadeIn(400);
+		$('.modal-wallpaper-show').on('click', function () {
+			$(this).fadeOut(400);
+		});
+		console.log(bg);
+	});
 });
