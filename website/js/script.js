2bdfdeccafe66c299b3aec76695c1bc306b4d8f0
erfiliman <53553718+erfiliman@users.noreply.github.com>
2019-08-02 01:33:22 +0700

    2 august

--- website/js/script.js
+++ website/js/script.js
@@ -3,14 +3,31 @@ $(function(){
 		$(this).find("ul:first").slideToggle();
 	});
 
+function nextBg(el) {
+	var bg = $(el).next().css('background-image');
+	bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+}
+
 	$('.wallpaper-item').on('click', function() {
 		var bg = $(this).css('background-image');
 		bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
 		$('.modal-wallpaper-show__item').attr('src', bg );
 		$('.modal-wallpaper-show').fadeIn(400);
 		$('.modal-wallpaper-show').on('click', function () {
-			$(this).fadeOut(400);
+			$(this).fadeOut(200);
 		});
 		console.log(bg);
+	});
+
+	$('.header-list__item_autho').on('click', function() {
+		var modal = $('.modal-autho');
+		var winH = $(window).height();
+		var winW = $(window).width();
+		modal.css('top', winH/2-modal.height()/2);
+		modal.css('left', winW/2-modal.width()/2);
+		$('.modal-autho').fadeIn(200);
+	});
+	$('.close-btn').on('click', function (){
+			$('.modal-autho').fadeOut(200);
 	});
 });
