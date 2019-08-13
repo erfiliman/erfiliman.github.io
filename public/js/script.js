b14237a93da35c329995f184ca4869e44480de47
erfiliman <53553718+erfiliman@users.noreply.github.com>
2019-08-13 12:29:34 +0700

    complete 0,8

--- public/js/script.js
+++ public/js/script.js
@@ -4,24 +4,15 @@ $(function(){
 
 	$( ".content-list__link" ).hover(
 		function() {
-			$( this ).prev('.content-image-item').first().css('border-color', '#00b31d');
+			$( this ).prev('.content-image').first().css('border-color', '#00b31d');
 		}, 
 		function() {
-			$( this ).prev('.content-image-item').first().css('border-color', '#797979');;
+			$( this ).prev('.content-image').first().css('border-color', '#797979');;
 		}
 	);
 
 	$(".mobile-menu").on("click", function() {
 		$(".list-menu").slideToggle("fast");
-	});
-
-	$(window).on("resize", function() {
-		if($(window).innerWidth()>480) {
-			$(".list-menu").css("display","inline-block");
-		}
-		if($(window).innerWidth()<480) {
-			$(".list-menu").css("display","flex");
-		}
 	});
 
 });
